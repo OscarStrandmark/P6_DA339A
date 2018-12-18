3675924f309c5cc5273df3ff634e7481d652e416
Oscar Strandmark <oscar.strandmark1@gmail.com>
2018-12-18 11:10:23 +0100

    Input (nästan) färdig. TODO: implementera funktioner för knappar

--- src/moment_2/Array7x7Input.java
+++ src/moment_2/Array7x7Input.java
@@ -4,101 +4,162 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
 public class Array7x7Input extends JFrame {
 
+	private JTextField[] southArray = new JTextField[7];
+	private JTextField[] eastArray = new JTextField[7];
+
+	private JButton btn_ReadRow = new JButton("Read row");
+	private JButton btn_WriteRow = new JButton("Write row");
+	
+	private JButton btn_ReadCol = new JButton("Read column");
+	private JButton btn_WriteCol = new JButton("Write column");
+	
+	private JTextField JTFNorth = new JTextField();
+	private JTextField JTFSouth = new JTextField();
+	
 	public Array7x7Input() {
 		setLayout(new BorderLayout());
 		
-		JPanel centerPane = new JPanel(new GridLayout(2, 1));
 		
-		JPanel centerNorthPane = new JPanel(new GridLayout(3,1));
-		
-		JPanel CNNPane = new JPanel(new GridLayout(1,2));
+		JPanel centerPane = new JPanel(new GridLayout(2, 1)); //Centerpanelen
+		JPanel centerNorthPane = new JPanel(new GridLayout(3, 1)); //Norra centerpanelen
+		JPanel CNNPane = new JPanel(new GridLayout(1, 2));//Norra norra centerpanelen
 		
 		JLabel colNbr = new JLabel("Column nbr:");
-		JTextField JTFNorth = new JTextField();
+
 		CNNPane.add(colNbr);
 		CNNPane.add(JTFNorth);
 		
-		JButton CNCBtn = new JButton("Read column");
-		JButton CNSBtn = new JButton("Write column");
-		
 		centerNorthPane.add(CNNPane);
-		centerNorthPane.add(CNCBtn);
-		centerNorthPane.add(CNSBtn);
-		
-		JPanel centerSouthPane = new JPanel(new GridLayout(3,1));
-		
-		JPanel CSNPane = new JPanel(new GridLayout(1,2));
-		
+		centerNorthPane.add(btn_ReadCol);
+		centerNorthPane.add(btn_WriteCol);
+
+		JPanel centerSouthPane = new JPanel(new GridLayout(3, 1));
+
+		JPanel CSNPane = new JPanel(new GridLayout(1, 2));
+
 		JLabel rowNbr = new JLabel("Row nbr:");
-		JTextField JTFSouth = new JTextField();
 		CSNPane.add(rowNbr);
 		CSNPane.add(JTFSouth);
-		
-		JButton CSCBtn = new JButton("Read row");
-		JButton CSSBtn = new JButton("Write row");
-		
+
 		centerSouthPane.add(CSNPane);
-		centerSouthPane.add(CSCBtn);
-		centerSouthPane.add(CSSBtn);
-		
+		centerSouthPane.add(btn_ReadRow);
+		centerSouthPane.add(btn_WriteRow);
+
 		centerPane.add(centerNorthPane);
 		centerPane.add(centerSouthPane);
-		
-		//centerPane.setPreferredSize(new Dimension(500,300));
-		add(centerPane,BorderLayout.CENTER);
-		JPanel eastPane = new JPanel(new GridLayout(7,1));
-		
-		JTextField JTFE1 = new JTextField();
-		JTextField JTFE2 = new JTextField();
-		JTextField JTFE3 = new JTextField();
-		JTextField JTFE4 = new JTextField();
-		JTextField JTFE5 = new JTextField();
-		JTextField JTFE6 = new JTextField();
-		JTextField JTFE7 = new JTextField();
 
-		
-		eastPane.add(JTFE1);
-		eastPane.add(JTFE2);
-		eastPane.add(JTFE3);
-		eastPane.add(JTFE4);
-		eastPane.add(JTFE5);
-		eastPane.add(JTFE6);
-		eastPane.add(JTFE7);
+		add(centerPane, BorderLayout.CENTER);
+		JPanel eastPane = new JPanel(new GridLayout(7, 1));
+
+		eastArray[0] = new JTextField();
+		eastArray[1] = new JTextField();
+		eastArray[2] = new JTextField();
+		eastArray[3] = new JTextField();
+		eastArray[4] = new JTextField();
+		eastArray[5] = new JTextField();
+		eastArray[6] = new JTextField();
+
+		eastPane.add(eastArray[0]);
+		eastPane.add(eastArray[1]);
+		eastPane.add(eastArray[2]);
+		eastPane.add(eastArray[3]);
+		eastPane.add(eastArray[4]);
+		eastPane.add(eastArray[5]);
+		eastPane.add(eastArray[6]);
 
 		eastPane.setPreferredSize(new Dimension(40, 250));
-		
+
 		add(eastPane, BorderLayout.EAST);
 		JPanel southPane = new JPanel(new BorderLayout());
-		
-		JPanel JTFSPanel = new JPanel(new GridLayout(1,7));
-		JTextField JTFS1 = new JTextField();
-		JTextField JTFS2 = new JTextField();
-		JTextField JTFS3 = new JTextField();
-		JTextField JTFS4 = new JTextField();
-		JTextField JTFS5 = new JTextField();
-		JTextField JTFS6 = new JTextField();
-		JTextField JTFS7 = new JTextField();
 
-		JTFSPanel.add(JTFS1);
-		JTFSPanel.add(JTFS2);
-		JTFSPanel.add(JTFS3);
-		JTFSPanel.add(JTFS4);
-		JTFSPanel.add(JTFS5);
-		JTFSPanel.add(JTFS6);
-		JTFSPanel.add(JTFS7);
-		JTFSPanel.setPreferredSize(new Dimension(250,0));
-		
+		JPanel JTFSPanel = new JPanel(new GridLayout(1, 7));
+
+		southArray[0] = new JTextField();
+		southArray[1] = new JTextField();
+		southArray[2] = new JTextField();
+		southArray[3] = new JTextField();
+		southArray[4] = new JTextField();
+		southArray[5] = new JTextField();
+		southArray[6] = new JTextField();
+
+		JTFSPanel.add(southArray[0]);
+		JTFSPanel.add(southArray[1]);
+		JTFSPanel.add(southArray[2]);
+		JTFSPanel.add(southArray[3]);
+		JTFSPanel.add(southArray[4]);
+		JTFSPanel.add(southArray[5]);
+		JTFSPanel.add(southArray[6]);
+
+		JTFSPanel.setPreferredSize(new Dimension(250, 0));
+
 		JPanel paddingSouth = new JPanel();
-		paddingSouth.setPreferredSize(new Dimension(40,40));
-		
-		southPane.add(JTFSPanel,BorderLayout.CENTER);
-		southPane.add(paddingSouth,BorderLayout.EAST);
-		
+		paddingSouth.setPreferredSize(new Dimension(40, 40));
+
+		southPane.add(JTFSPanel, BorderLayout.CENTER);
+		southPane.add(paddingSouth, BorderLayout.EAST);
+
 		add(southPane, BorderLayout.SOUTH);
+		
+		addListeners();
+	}
+
+	private void addListeners() {
+		ButtonListener listener = new ButtonListener();
+		btn_ReadCol.addActionListener(listener);
+		btn_WriteCol.addActionListener(listener);
+		btn_ReadRow.addActionListener(listener);
+		btn_WriteRow.addActionListener(listener);
+	}
+	
+	/**
+	 * Hämtar JTextField i angivna positionen i den södra array
+	 * @param pos
+	 * @return JTextField
+	 */
+	public JTextField accessSouthArray(int pos) {
+		return southArray[pos];
+	}
+
+	/**
+	 * Hämtar JTextField i angivna positionen i den östra array
+	 * @param pos
+	 * @return JTextField
+	 */
+	public JTextField accessEastArray(int pos) {
+		return eastArray[pos];
+	}
+	
+	class ButtonListener implements ActionListener {
+
+		public ButtonListener() {
+			
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource().equals(btn_ReadCol)) {
+				System.out.println("readcol");
+			}
+			
+			if(e.getSource().equals(btn_WriteCol)) {
+				System.out.println("writecol");
+			}
+			
+			if(e.getSource().equals(btn_ReadRow)) {
+				System.out.println("readrow");
+			}
+			
+			if(e.getSource().equals(btn_WriteRow)) {
+				System.out.println("writerow");
+			}
+		}
+		
 	}
 }
