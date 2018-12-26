cc0220ddde21d7683a37249366243edb04722d46
Oscar Strandmark <oscar.strandmark1@gmail.com>
2018-12-26 18:28:06 +0100

    Basfiler i moment 3

---
+++ src/moment_3/Controller.java
@@ -0,0 +1,12 @@
+package moment_3;
+
+import moment_1.Array7;
+import moment_1.Array7x7;
+
+public class Controller {
+
+	private Test2UI 		ui;
+	private Array7x7 	 array;
+	private Array7  leftColumn;
+	private Array7 rightColumn;
+}
---
+++ src/moment_3/Test2UI.java
@@ -0,0 +1,10 @@
+package moment_3;
+
+public class Test2UI {
+
+	private Controller controller;
+	
+	public static void main(String[] args) {
+		
+	}
+}
